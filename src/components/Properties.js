2ad8009f989fb000852d555142f88d821ab36724
Conceptx <theophilusokoye6@gmail.com>
2019-04-14 14:06:17 +0200

    rendering properties from algolia, instantsearch, infinite flatlist

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,176 +1,114 @@
-import React, { Fragment } from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, FlatList, ScrollView, TouchableWithoutFeedback } from 'react-native';
 import { Appbar, Text, Button, Switch, Searchbar, Card } from 'react-native-paper';
 import Stars from 'react-native-stars';
 import { Ionicons } from '@expo/vector-icons';
+import { InstantSearch } from 'react-instantsearch/native';
+import { connectSearchBox, connectInfiniteHits } from 'react-instantsearch/connectors';
 
+const SearchBox = connectSearchBox(({ refine, currentRefinement }) => {
+	return (
+		<Searchbar
+			placeholder="Search eg unlimited WIFI"
+			value={currentRefinement}
+			onChangeText={text => refine(text)}
+			style={styles.SearchBar}
+			clearButtonMode="always"
+			spellCheck={false}
+			autoCorrect={false}
+			autoCapitalize="none"
+		/>
+	);
+});
+
+const Results = connectInfiniteHits(({ hits, hasMore, refine }) => {
+	const onEndReached = () => {
+		if (hasMore) {
+			refine();
+		}
+	};
+	return (
+		<FlatList
+			data={hits}
+			onEndReached={onEndReached}
+			keyExtractor={property => property.objectID}
+			renderItem={({ item }) => {
+				console.log(item);
+				return hits === [] ? <Text>No Houses Found</Text> : <Property pty={item} />;
+			}}
+		/>
+	);
+});
+
+const Property = ({ pty }) => {
+	return (
+		<TouchableWithoutFeedback>
+			<Card>
+				<Card.Title subtitle={pty.location} />
+				<Card.Cover
+					source={{
+						uri: pty.media[0]
+					}}
+				/>
+				<Card.Content>
+					<View style={styles.Filters}>
+						<Stars
+							display={pty.overallRating}
+							spacing={8}
+							count={5}
+							starSize={20}
+							backingColor="white"
+							fullStar={<Ionicons name="md-star" size={20} color="black" />}
+							emptyStar={<Ionicons name="md-star" size={20} color="white" />}
+						/>
+						<Text>{pty.verified === false ? 'Not Verified' : 'Verified'}</Text>
+					</View>
+					<View>
+						<Text style={styles.Info}>
+							{pty.shared === true ? 'Shared - ' : ''} {pty.room.length} rooms
+						</Text>
+						<Text style={styles.Info}>
+							${pty.minPrice} - ${pty.maxPrice} / month
+						</Text>
+					</View>
+				</Card.Content>
+			</Card>
+		</TouchableWithoutFeedback>
+	);
+};
 const Properties = () => (
-	<Fragment>
-		<Appbar.Header style={styles.AppBarTheme}>
-			<Appbar.BackAction onPress={() => console.log('Going Back')} />
-			<Appbar.Content title="Khaya" subtitle="Home" />
-			<Appbar.Action icon="menu" onPress={() => console.log('See Options')} />
-		</Appbar.Header>
-		<View style={styles.Container}>
-			<Text style={{ marginBottom: -30 }}>Show Map</Text>
-			<View style={styles.Filters}>
-				<Switch
-					color={'#000000'}
-					value={false}
-					onValueChange={() => console.log('Switch Toggled')}
-				/>
-				<Button
-					color={'#ffffff'}
-					style={styles.Button}
-					mode="outlined"
-					onPress={() => console.log('Pressed')}>
-					Apply Filters
-				</Button>
-			</View>
-			<Searchbar
-				placeholder="Search ex: unlimited WIFI"
-				onChangeText={query => console.log('Searched here')}
-				style={styles.SearchBar}
-			/>
-		</View>
+	<InstantSearch
+		appId="D97UPSIQ04"
+		apiKey="4aab387d93c9f7129edfe6d2bda44ff0"
+		indexName="Properties">
 		<ScrollView>
-			<Card>
-				<Card.Title subtitle="15 Skymaster Drive, Belvedere, Harare" />
-				<Card.Cover
-					source={{
-						uri:
-							'https://images.pexels.com/photos/2063258/pexels-photo-2063258.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-					}}
-				/>
-				<Card.Content>
-					<View style={styles.Filters}>
-						<Stars
-							display={4.0}
-							spacing={8}
-							count={5}
-							starSize={20}
-							backingColor="white"
-							fullStar={<Ionicons name="md-star" size={20} color="black" />}
-							emptyStar={<Ionicons name="md-star" size={20} color="white" />}
-						/>
-						<Text>Not Verified</Text>
-					</View>
-					<View>
-						<Text style={styles.Info}>Shared - 9 rooms</Text>
-						<Text style={styles.Info}>$100 - $150 / month</Text>
-					</View>
-				</Card.Content>
-			</Card>
-			<Card>
-				<Card.Title subtitle="15 Skymaster Drive, Belvedere, Harare" />
-				<Card.Cover
-					source={{
-						uri:
-							'https://images.pexels.com/photos/2063258/pexels-photo-2063258.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-					}}
-				/>
-				<Card.Content>
-					<View style={styles.Filters}>
-						<Stars
-							display={4.0}
-							spacing={8}
-							count={5}
-							starSize={20}
-							backingColor="white"
-							fullStar={<Ionicons name="md-star" size={20} color="black" />}
-							emptyStar={<Ionicons name="md-star" size={20} color="white" />}
-						/>
-						<Text>Not Verified</Text>
-					</View>
-					<View>
-						<Text style={styles.Info}>Shared - 9 rooms</Text>
-						<Text style={styles.Info}>$100 - $150 / month</Text>
-					</View>
-				</Card.Content>
-			</Card>
-			<Card>
-				<Card.Title subtitle="15 Skymaster Drive, Belvedere, Harare" />
-				<Card.Cover
-					source={{
-						uri:
-							'https://images.pexels.com/photos/2063258/pexels-photo-2063258.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-					}}
-				/>
-				<Card.Content>
-					<View style={styles.Filters}>
-						<Stars
-							display={4.0}
-							spacing={8}
-							count={5}
-							starSize={20}
-							backingColor="white"
-							fullStar={<Ionicons name="md-star" size={20} color="black" />}
-							emptyStar={<Ionicons name="md-star" size={20} color="white" />}
-						/>
-						<Text>Not Verified</Text>
-					</View>
-					<View>
-						<Text style={styles.Info}>Shared - 9 rooms</Text>
-						<Text style={styles.Info}>$100 - $150 / month</Text>
-					</View>
-				</Card.Content>
-			</Card>
-			<Card>
-				<Card.Title subtitle="15 Skymaster Drive, Belvedere, Harare" />
-				<Card.Cover
-					source={{
-						uri:
-							'https://images.pexels.com/photos/2063258/pexels-photo-2063258.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-					}}
-				/>
-				<Card.Content>
-					<View style={styles.Filters}>
-						<Stars
-							display={4.0}
-							spacing={8}
-							count={5}
-							starSize={20}
-							backingColor="white"
-							fullStar={<Ionicons name="md-star" size={20} color="black" />}
-							emptyStar={<Ionicons name="md-star" size={20} color="white" />}
-						/>
-						<Text>Not Verified</Text>
-					</View>
-					<View>
-						<Text style={styles.Info}>Shared - 9 rooms</Text>
-						<Text style={styles.Info}>$100 - $150 / month</Text>
-					</View>
-				</Card.Content>
-			</Card>
-			<Card>
-				<Card.Title subtitle="15 Skymaster Drive, Belvedere, Harare" />
-				<Card.Cover
-					source={{
-						uri:
-							'https://images.pexels.com/photos/2063258/pexels-photo-2063258.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-					}}
-				/>
-				<Card.Content>
-					<View style={styles.Filters}>
-						<Stars
-							display={4.0}
-							spacing={8}
-							count={5}
-							starSize={20}
-							backingColor="white"
-							fullStar={<Ionicons name="md-star" size={20} color="black" />}
-							emptyStar={<Ionicons name="md-star" size={20} color="white" />}
-						/>
-						<Text>Not Verified</Text>
-					</View>
-					<View>
-						<Text style={styles.Info}>Shared - 9 rooms</Text>
-						<Text style={styles.Info}>$100 - $150 / month</Text>
-					</View>
-				</Card.Content>
-			</Card>
+			<Appbar.Header style={styles.AppBarTheme}>
+				<Appbar.BackAction onPress={() => console.log('Going Back')} />
+				<Appbar.Content title="Khaya" subtitle="Home" />
+				<Appbar.Action icon="menu" onPress={() => console.log('See Options')} />
+			</Appbar.Header>
+			<View style={styles.Container}>
+				<Text style={{ marginBottom: -30 }}>Show Map</Text>
+				<View style={styles.Filters}>
+					<Switch
+						color={'#000000'}
+						value={false}
+						onValueChange={() => console.log('Switch Toggled')}
+					/>
+					<Button
+						color={'#ffffff'}
+						style={styles.Button}
+						mode="outlined"
+						onPress={() => console.log('Pressed')}>
+						Apply Filters
+					</Button>
+				</View>
+				<SearchBox />
+			</View>
+
+			<Results />
 		</ScrollView>
-	</Fragment>
+	</InstantSearch>
 );
 
 const styles = StyleSheet.create({
