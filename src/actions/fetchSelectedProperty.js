080a44572f622be9ec00f3daba0aa63b58502a8d
Conceptx <theophilusokoye6@gmail.com>
2019-04-20 10:41:48 +0200

    changed async fetchSelectedProperty to sync action

--- src/actions/fetchSelectedProperty.js
+++ src/actions/fetchSelectedProperty.js
@@ -3,15 +3,8 @@ import { FETCH_SELECTED_PROPERTY } from './types';
 export const fetchSelectedProperty = id => dispatch => {
 	console.log('In fetch selected property action ...');
 
-	fetch(`https://khaya-api.herokuapp.com/property/${id}/details`)
-		.then(res => res.json())
-		.then(res => {
-			const { success, ...property } = res;
-
-			dispatch({
-				type: FETCH_SELECTED_PROPERTY,
-				payload: property
-			});
-		})
-		.catch(error => console.log(error));
+	dispatch({
+		type: FETCH_SELECTED_PROPERTY,
+		payload: id
+	});
 };
